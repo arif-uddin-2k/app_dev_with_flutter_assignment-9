2353bc44c5ab342eac8a01c6784124f556a77809
mahmud5658 <almahmuda5@gmail.com>
2024-08-21 20:18:43 +0600

    live score

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todo/ui/screen/match_list_screen.dart';
+
+class LiveScore extends StatelessWidget {
+  const LiveScore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
